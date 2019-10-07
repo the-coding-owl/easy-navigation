6af24f12c17ac755cc9886ea7beaff8bd5c85ac1
Kevin Ditscheid <kevin.ditscheid@sgalinski.de>
2019-10-07 21:51:00 +0200

    [TASK] Change license headers in PHP files and fix composer.json formatting

--- Classes/UserFunc.php
+++ Classes/UserFunc.php
@@ -1,16 +1,20 @@
 <?php
-
 /*
- * This file is part of the TYPO3 CMS project.
+ * the-coding-owl/easy-navigation
+ * Copyright (C) 2019 Kevin Ditscheid <kevin@the-coding-owl.de>
  *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace TheCodingOwl\EasyNavigation;
