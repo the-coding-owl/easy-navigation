3f6962d5685f8281489e816cd0348c10be7ee312
Kevin Ditscheid <kevin@the-coding-owl.de>
2023-05-22 23:26:51 +0200

    [TASK] Raise version to 2.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,10 +28,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '2.1.0',
+	'version' => '2.2.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '9.5.0-11.5.99'
+			'typo3' => '9.5.0-12.4.99'
 		),
 		'conflicts' => array(
 		),
